7522b31a406c6a10196a8912ca6f6682555b5378
Azam <azamkhan561@gmail.com>
2024-03-17 16:56:45 +0530

    google form testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Form from './components/Form/Form';
 import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Form />
     </div>
   );
 }
---
+++ src/components/Form/Form.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Form.scss'
+
+function Form() {
+  return (
+    <div>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe8ihhotKhUu77nf5JdGngPP6e4fhD95MCUnNNDnrO6-xV19w/viewform?embedded=true" width="640" height="705" frameborder="0" marginheight="0" marginwidth="0" />
+    </div>
+  )
+}
+
+export default Form

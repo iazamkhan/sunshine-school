c98a43d3850a1c6ded3df3344dea5694aa81e73b
Azam <azamkhan561@gmail.com>
2024-03-19 09:31:31 +0530

    form is successfully sending data to google sheets

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import axios from 'axios'
 import './Form.scss'
 
@@ -6,18 +6,33 @@ function Form() {
 
     const url = "https://script.google.com/macros/s/AKfycbwPvAI_tRl51i82sq6Re3nbGWEtdlJ0CODqbCAZk9XDSLAsN3RTs14Ye_od1MrbNYbfWw/exec"
     const formRef = useRef(null)
+    const [loading, setLoading] = useState(false)
 
-    
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        setLoading(true)
+
+        fetch(url, {
+        method: 'POST', 
+        body: new FormData(formRef.current),
+
+    }).then(res => {
+            console.log("SUCCESSFULLY SUBMITTED")
+            setLoading(false)
+        })
+        .catch(err => console.log(err))
+    }
+
     return (
         <div>
             <form ref={formRef} className="form" action="https://docs.google.com/forms/d/e/1FAIpQLSe8ihhotKhUu77nf5JdGngPP6e4fhD95MCUnNNDnrO6-xV19w/viewform?usp=sf_link" method="POST">
                 <div className="name-and-class">
                     <label for="entry.300882085">STUDENT NAME
-                        <input type="text" name="entry.300882085" required="required" />
+                        <input type="text" name="STUDENT NAME" required="required" />
                     </label>
                     <label for="entry.1163096108">
                         CLASS
-                        <select className="select-class" required="required" name="entry.1163096108">
+                        <select className="select-class" required="required" name="CLASS">
                             <option value="KG">KG</option>
                             <option value="NURSERY">NURSERY</option>
                             <option value="I">I</option>
@@ -31,7 +46,7 @@ function Form() {
                         </select>
                     </label>
                 </div>
-                <button type="submit">Submit</button>
+                <button type="submit" onClick={handleSubmit}>Submit</button>
             </form>
         </div>
     )
